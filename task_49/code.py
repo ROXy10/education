0accd63d490eac3795d1c701d97f5748bbb1fd71
ROXy <roxy_10@mail.ru>
2016-03-21 11:56:20 +0200

    init

--- task_49/code.py
+++ task_49/code.py
@@ -3,7 +3,8 @@ def code(text, key=1):
     symbol = " !@#$%^&*()_+~|"
     i = 0
     while i < len(text)-1:
-
+        print(text[i])
         i += 1
+
 
 print(code("Агент 007, срочно выйдите на связь"))
